bc3a8164a36a7354135ceba036cfcc5c7e75e4bc
Annyversary <yannick.oefler@t-online.de>
2025-05-10 16:27:31 +0200

    Created separate monthly test splits

--- src/utils/data_splits/create_test_data.py
+++ src/utils/data_splits/create_test_data.py
@@ -1,25 +1,24 @@
 import os
 import torch
 
-from torch_geometric.data import Batch
 from helper_functions.load_graphml_files import load_graphml_files
 from normalize_data.normalize_test_data import normalize_test_features
-from helper_functions.print_datasplit_info import print_batch_shape
 
 def main(years=[2024]):
-    save_dir = os.path.join("data", "data_splits", "2021_to_2023_split")
+    save_dir = os.path.join("data", "data_splits", "test_data_monthly")
     os.makedirs(save_dir, exist_ok=True)
-    test_save_path = os.path.join(save_dir, "test_data.pt") 
 
     test_data_list = load_graphml_files(years)
-    test_data_batch = Batch.from_data_list(test_data_list)
-    test_data_batch = normalize_test_features(test_data_batch)
-    print("\nTest Data Statistics:")
-    print_batch_shape(test_data_batch)
 
-    torch.save(test_data_batch, test_save_path)
-    print(f"\nTest data saved to: {test_save_path}")
+    # Optional: normalize each Data object individually
+    for i, data in enumerate(test_data_list):
+        data = normalize_test_features(data)
+        print(f"\nStatistics for month {i+1:02d}:")
 
+        # Save each file individually
+        month_save_path = os.path.join(save_dir, f"month_{i+1:02d}.pt")
+        torch.save(data, month_save_path)
+        print(f"Saved to: {month_save_path}")
 
 if __name__ == "__main__":
     main()
